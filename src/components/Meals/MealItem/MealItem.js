977449f5dab36ebd77f5e1ff9e135a9d7b4100f4
sadaf0611 <122720925+sadaf0611@users.noreply.github.com>
2023-09-26 20:41:05 +0530

    Add button with order count

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import classes from'./MealItem.module.css'
+import MealItemForm from './MealItemForm';
+
 const MealItem = (props) => {
-    const price=`$${props.price.toFixed(2)}`
+    const price=`$${props.price.toFixed(2)}`;
   return (
     <li className={classes.meal}>
     <div>
@@ -9,7 +11,9 @@ const MealItem = (props) => {
       <div className={classes.description}>{props.description}</div>
       <div className={classes.price}>{price}</div>
     </div>
-    <div></div>
+    <div>
+      <MealItemForm/>
+    </div>
     </li>
   )
 }
